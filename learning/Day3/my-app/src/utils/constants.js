053543b1610b91bb39137c552823cf055d16c7d9
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-04-30 21:41:22 +0530

    fer

--- learning/Day3/my-app/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const LOGO_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_940,h_560/swiggy_one/dweb_swiggy_one";
-
-export const CLOUD_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
-  
- 
